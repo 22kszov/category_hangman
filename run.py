6207a1c41ec0ff0f2107e74103be6e840c4b7407
22kszov <22kszov@gmail.com>
2024-03-03 01:37:14 +0000

    Add main menu function and welcome message

---
+++ run.py
@@ -0,0 +1,18 @@
+"""
+Categories Hangman
+"""
+
+from words import sports
+from words import vegetbales
+from words import occupations
+
+
+def main_menu():
+    """
+    Displays sarting screen to the game.
+    All other functions are executed from here.
+    """
+    print("Welcome to Categories Hangman\n")
+
+
+main_menu()
--- words.py
+++ words.py
@@ -1,3 +1,6 @@
+"""
+Module providing lists for word selection categories.
+"""
 sports = ["football", "basketball", "boxing", "hockey", "track", "javelin"]
 vegetbales = ["carrot", "broccoli", "artichoke",
               "beetroot", "tomato", "cucumber"]
