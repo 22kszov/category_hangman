6a042bef5ce64f2f3b9b10961d0687e9924e3f28
22kszov <22kszov@gmail.com>
2024-03-03 01:29:21 +0000

    Add words file with lists of words for categories

---
+++ words.py
@@ -0,0 +1,5 @@
+sports = ["football", "basketball", "boxing", "hockey", "track", "javelin"]
+vegetbales = ["carrot", "broccoli", "artichoke",
+              "beetroot", "tomato", "cucumber"]
+occupations = ["banker", "plumber", "chef",
+               "bricklayer", "electrician", "devloper"]
